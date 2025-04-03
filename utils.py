b3d94921b7ec76af67a6804d82456b018f864212
teekuningas <erkkahe@gmail.com>
2025-04-03 05:48:52 +0000

    Support vtts as data

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+import re
 import os
+
 
 def read_files(folder, prefix):
     contents = []
@@ -9,3 +11,22 @@ def read_files(folder, prefix):
         with open(path, 'r') as f:
             contents.append((fname, "".join(f.readlines())))
     return contents
+
+
+def strip_webvtt_to_plain_text(webvtt_string):
+    # Remove WEBVTT header
+    webvtt_string = re.sub(r'^WEBVTT\s*\n', '', webvtt_string, flags=re.MULTILINE)
+    
+    # Remove timestamps
+    webvtt_string = re.sub(r'\d{2}:\d{2}.\d{3} --> \d{2}:\d{2}.\d{3}\s*\n', '', webvtt_string)
+    
+    # Remove speaker tags
+    webvtt_string = re.sub(r'\[SPEAKER_\d{2}\]:\s', '', webvtt_string)
+    
+    # Replace multiple newlines with double newline (for better readability)
+    webvtt_string = re.sub(r'\n\n+', '\n\n', webvtt_string)
+    
+    # Strip leading/trailing whitespace from start and end
+    webvtt_string = webvtt_string.strip()
+    
+    return webvtt_string
