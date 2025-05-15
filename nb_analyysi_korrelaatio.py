8a9590c03d9bcb50bb516e9e5d39bc20bfa1f78b
teekuningas <erkkahe@gmail.com>
2025-05-15 09:11:03 +0000

    Add initial nb_analyysi_korrelaatio.py

---
+++ nb_analyysi_korrelaatio.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# This notebook runs a correlation analysis for textual interview data and metadata such as location type
+
+# %%
+print("Hello world!")
+
+# %%
